8f1406c0badb8e3f81f3fb2548529e70a0a53be9
Clement <clemesoucy@gmail.com>
2018-06-21 10:38:40 -0400

    Config ADE fonctionnel

--- ADE9000/Code arduino test/sketch_jun07a/sketch_jun07a.ino
+++ ADE9000/Code arduino test/sketch_jun07a/sketch_jun07a.ino
@@ -19,6 +19,10 @@
 #define VLEVEL_CMD  0x40F0
 #define EP_CFG_CMD_WRITE  0x4B00
 #define EP_CFG_READ 0x4B08
+#define CONFIG0_CMD_READ  0x0608
+#define CONFIG0_CMD_WRITE 0x0600
+
+#define CONV 0.000000007450580597
 
 float THD=0;  //en %
 float PF=0;
@@ -26,16 +30,16 @@ float PF=0;
 //Les constantes et variables pour le arduino
 #define chipSelect 6
 #define TIMER_LECTURE 1000  //en ms
-long last_millis=0; //permet de faire comme un timer qui ne bloque pas le code (voir fonction millis() d'arduino)
-unsigned int temp=0;
+unsigned long last_millis=0; //permet de faire comme un timer qui ne bloque pas le code (voir fonction millis() d'arduino)
+unsigned long temp=0;
 unsigned short temp16=0;
 unsigned short dsp_state=0;
 
 //Les prototypes
 void afficher_resultats();  //propre au arduino
-unsigned int lecture_registre(short nb_bytes,unsigned short cmd_hdr); //dequoi de similaire pour notre application Atmel
+unsigned long lecture_registre(short nb_bytes,unsigned short cmd_hdr); //dequoi de similaire pour notre application Atmel
 void enable_dsp();  //enable le dsp (RUN=1)
-void ecriture_registre(short nb_bytes,unsigned short cmd_hdr,unsigned int val);
+void ecriture_registre(short nb_bytes,unsigned short cmd_hdr,unsigned long val);
 
 void setup() {
   Serial.begin(9600); //pour la console avec arduino
@@ -48,20 +52,18 @@ void setup() {
   
   //Configuration du ADE9000
   temp16=lecture_registre(2,ACCMODE_CMD_READ);  //config fréquence
-  ecriture_registre(2,ACCMODE_CMD_WRITE,temp16|0x0100);
+  ecriture_registre(2,ACCMODE_CMD_WRITE,(temp16|0x0100));
   
   ecriture_registre(4,VLEVEL_CMD,0x00117514);//config VLEVEL nominal (1Vp)
   
   temp16=lecture_registre(2,EP_CFG_READ);//disable no load timer
-  ecriture_registre(2,EP_CFG_CMD_WRITE,temp16|0xE000);
+  ecriture_registre(2,EP_CFG_CMD_WRITE,(temp16|0xE000));
   
   enable_dsp(); //dernière étape de la config du ADE9000
 
-  temp=lecture_registre(2,LAST_CMD);
+  temp=lecture_registre(2,LAST_CMD);  //2 étapes inutiles pour voir le fonctionnement au logic analyser
   temp=lecture_registre(2,RUN_CMD);
-  temp=lecture_registre(2,LAST_CMD);
-  temp=lecture_registre(4,0x4728);  //lecture bidon pour vérifier le fonctionnement
-  temp=lecture_registre(2,LAST_CMD);
+
   
 }
 
@@ -71,10 +73,10 @@ void loop() {
   if (millis()>last_millis+TIMER_LECTURE) //vérification qui s'apparente à un Timer pour un environnement autre que Arduino
   {
     temp=lecture_registre(4,PF_CMD);
-    PF=(float)temp/(float)134217728;  //conformément au fichier .cpp du code du shield de l'ADE9000
+    PF=(float)temp * CONV;
     delay(1);
     temp=lecture_registre(4,THD_CMD);
-    THD=(float)temp*100/(float)134217728;  //conformément au fichier .cpp du code du shield de l'ADE9000
+    THD=(float)temp * 100 * CONV;
     delay(1);
     dsp_state=lecture_registre(2,RUN_CMD);
     delay(1);
@@ -95,30 +97,30 @@ void enable_dsp()// (RUN=1)
   SPI.transfer(0x01); //enable DSP for mesurement
   digitalWrite(chipSelect,HIGH);
 }
-void ecriture_registre(short nb_bytes,unsigned short cmd_hdr,unsigned int val)//faire quelque chose de similaire pour notre application
+void ecriture_registre(short nb_bytes,unsigned short cmd_hdr,unsigned long val)
 {
   // cmd_hdr: 16bits non signé incluant le bit3=0 pour le write
-  //val: 32bits non signé contenant la valeur à inscrire. Si 16 bits à écrire: 0xXXXX3210 où X est un don't care
+  //val: 32 bits où les 16 msb sont des don't care si nb_bytes
   byte msb=cmd_hdr>>8;
   byte lsb=(cmd_hdr<<8)>>8;
+  
   byte byte1=0;
   byte byte2=0;
   byte byte3=0;
   byte byte4=0;
-  
-  if (nb_bytes==2)  //isole les bytes et supprime les don't care qui seraient non-nul
+
+  if (nb_bytes==2)
   {
-    byte1=(val<<16)>>24;
-    byte2=(val<<24)>>24;
+    byte1=val>>8;//isole les bytes et supprime les don't care qui seraient non-nul
+    byte2=(val<<8)>>8;
   }
-  else if(nb_bytes==4)  //isole les bytes
+  else
   {
-    byte1=val>>24;
+    byte1=val>>24;//isole les bytes et supprime les don't care qui seraient non-nul
     byte2=(val<<8)>>24;
     byte3=(val<<16)>>24;
     byte4=(val<<24)>>24;
   }
-
   digitalWrite(chipSelect,LOW);
   SPI.transfer(msb);  //le command header
   SPI.transfer(lsb);
@@ -132,11 +134,11 @@ void ecriture_registre(short nb_bytes,unsigned short cmd_hdr,unsigned int val)//
   }
   digitalWrite(chipSelect,HIGH);
 }
-unsigned int lecture_registre(short nb_bytes,unsigned short cmd_hdr)  //faire quelque chose de similaire pour notre application
+unsigned long lecture_registre(short nb_bytes,unsigned short cmd_hdr)  //faire quelque chose de similaire pour notre application
 {
   // cmd_hdr: 16bits non signé incluant le bit3=1 pour le read
   
-  unsigned int result=0;
+  unsigned long result=0;
   byte newbyte=0;
   byte msb=cmd_hdr>>8;
   byte lsb=(cmd_hdr<<8)>>8;
