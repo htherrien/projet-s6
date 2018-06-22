b41ed5a0f30c6c8e88fb37976b7755801acad557
Hugo Therrien <hugo.therrien2@usherbrooke.ca>
2018-06-21 20:07:13 -0400

    Docuemented the address macros

--- GccBoardProject1/GccBoardProject1/include/ADE9000/Addresses.h
+++ GccBoardProject1/GccBoardProject1/include/ADE9000/Addresses.h
@@ -12,9 +12,15 @@
 #pragma once
 
 /* Measurements addresses */
-#define ADDR_APF 0x216   /* Phase A Power Factor, updated every 1.024s. */
-#define ADDR_AVTHD 0x217 /* Phase A voltage THD, updated every 1.024s. */
+
+/** Phase A Power Factor, updated every 1.024s. */
+#define ADDR_APF 0x216
+/** Phase A voltage THD, updated every 1.024s. */
+#define ADDR_AVTHD 0x217 
 
 /* Control addresses */
-#define ADDR_RUN 0x480      /* Write 1 to start the measurements, 0 to stop. */
-#define ADDR_LAST_CMD 0x4AE /* Address of the last command. */
+
+/** Write 1 to this address to start the measurements, 0 to stop. */
+#define ADDR_RUN 0x480 
+/** Reading this address will yield the address of the last command. */     
+#define ADDR_LAST_CMD 0x4AE 
