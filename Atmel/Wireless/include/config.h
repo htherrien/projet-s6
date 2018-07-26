9c51b8e208f16c5f7e89155d505884395c0ec79b
Danaë Dufour-Forget <dufd2703@usherbrooke.ca>
2018-07-26 11:18:45 -0400

    Added source files and includes for the lightweight mesh to work. It compiles.

---
+++ Atmel/Wireless/include/config.h
@@ -0,0 +1,14 @@
+/*
+ * config.h
+ *
+ * Created: 2018-07-26 11:04:15
+ *  Author: dufd2703
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+
+#endif /* CONFIG_H_ */
---
+++ Atmel/Wireless/include/wireless.h
@@ -0,0 +1,16 @@
+/*
+ * wireless.h
+ *
+ * Created: 2018-07-26 10:35:15
+ *  Author: dufd2703
+ */ 
+
+
+#ifndef WIRELESS_H_
+#define WIRELESS_H_
+
+
+
+
+
+#endif /* WIRELESS_H_ */
