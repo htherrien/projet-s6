79c7d19ed970cdcebdc6507d19b09ade4026ac87
Hugo Therrien <hugo.therrien2@usherbrooke.ca>
2018-07-28 11:08:55 -0400

    Cleaned wireless project

---
+++ Atmel/Wireless/config/config.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ *
+ * Copyright (c) Projet S6, Équipe P4
+ *
+ ******************************************************************************/
+
+/**
+ * @file config.h
+ * @brief Lightweight mesh network stack configuration file
+ */
+
+#pragma once
