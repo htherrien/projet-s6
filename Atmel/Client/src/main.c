bb2dd3877aaf376507b8e0885835cc1450da5b4b
Hugo Therrien <hugo.therrien2@usherbrooke.ca>
2018-07-28 11:09:59 -0400

    Cleaned Client project

--- Atmel/Client/src/main.c
+++ Atmel/Client/src/main.c
@@ -1,40 +1,15 @@
-/**
- * \file
+/*******************************************************************************
  *
- * \brief Empty user application template
+ * Copyright (c) Projet S6, Équipe P4
  *
- */
+ ******************************************************************************/
 
 /**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
+ * @file main.c
+ * @brief Wireless power analyzer measuring station's board main loop.
  */
 
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
-#include <asf.h>
-
-int main (void)
+int main(void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
-
-	board_init();
-
-	/* Insert application code here, after the board has been initialized. */
+    return 1;
 }
