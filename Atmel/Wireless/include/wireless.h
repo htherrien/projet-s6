77590e16bf9a5f5399f606fecf7d0eb6f5875d7a
Hugo Therrien <theh2203@usherbrooke.ca>
2018-07-29 13:53:18 -0400

    Changed banner and ifguard to pragma once

--- Atmel/Wireless/include/wireless.h
+++ Atmel/Wireless/include/wireless.h
@@ -1,13 +1,10 @@
-/*
- * wireless.h
+/*******************************************************************************
  *
- * Created: 2018-07-29 11:22:54 AM
- *  Author: danae
- */ 
+ * Copyright (c) Projet S6, Équipe P4
+ *
+ ******************************************************************************/
 
-
-#ifndef WIRELESS_H_
-#define WIRELESS_H_
+#pragma once
 
 #include <stdint.h>
 
@@ -20,8 +17,3 @@ uint8_t getWirelessLQI(void);
 uint8_t getWirelessRSSI(void);
 void write_Wireless(uint8_t *data, uint8_t size);
 void PHY_TaskHandler(void);
-
-
-
-
-#endif /* WIRELESS_H_ */
