4e80bc7c74e3806fb1bbaa822ab9388b8338a58a
Hugo Therrien <hugo.therrien2@usherbrooke.ca>
2018-07-30 13:15:05 -0400

    Moved timers to functions

---
+++ Atmel/Wireless/include/Timers.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *
+ * Copyright (c) Projet S6, Équipe P4
+ *
+ ******************************************************************************/
+
+#pragma once
+
+#include <stdbool.h>
+
+void setupWirelessTimers(void);
+
+void resetWirelessTimers(void)
+
+bool checkSendDataTimer(void);
+
+bool checkAckTimer(void);
